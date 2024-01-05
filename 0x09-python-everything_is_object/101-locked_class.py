229434f6a17e906a1809c0ea5d1e22008d3f7c00
Gloriah-B <Gloriah-B@github.com>
2024-01-05 10:34:21 -0800

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a locked class."""
+
+
+class LockedClass:
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+        self.__dict__[name] = value
