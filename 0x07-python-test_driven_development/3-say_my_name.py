5fa3fd616fbff01d440c63b14cb792a41ea71d81
Gloriah-B <Gloriah-B@github.com>
2024-01-08 08:12:09 -0800

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
-"""Prints a name."""
-
-
 def say_my_name(first_name, last_name=""):
-    """Prints the name in a specified format.
+    """
+    Prints the name in a specified format.
 
     Args:
         first_name (str): First name.
@@ -12,8 +9,12 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If first_name or last_name is not a string.
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if last_name and not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
-    full_name = "My name is {} {}".format(first_name, last_name) if last_name else "My name is {}".format(first_name)
+    full_name = f"My name is {first_name}"
+    if last_name:
+        full_name += f" {last_name}"
     print(full_name)
