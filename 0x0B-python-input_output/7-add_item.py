0d628999dbfdea576b11cf9ca689a25650182f9a
Gloriah-B <Gloriah-B@github.com>
2024-01-09 10:24:55 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-"""
-Module: json_operations
 
-Provides functionality to save and load Python objects as JSON in a file.
-"""
+"""Add all arguments to a Python list and save them to a file."""
 
 
-import json
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """
-    Save to JSON File Function
-
-    Writes the JSON representation of the provided object to a file.
-
-    Args:
-        my_obj: The object to be serialized and saved to a JSON file
-        filename (str): The name of the file to save the JSON representation
-
-    Returns:
-        None
-    """
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    """
-    Load from JSON File Function
-
-    Creates an object from a JSON file.
-
-    Args:
-        filename (str): The name of the JSON file to load
-
-    Returns:
-        object: Python data structure created from the JSON file
-    """
-    with open(filename, 'r') as file:
-        return json.load(file)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
