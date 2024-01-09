786ff4839ecc6de00f302e1c678791cc368f4d08
Gloriah-B <Gloriah-B@github.com>
2024-01-09 10:08:35 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -22,3 +22,19 @@ def save_to_json_file(my_obj, filename):
     """
     with open(filename, 'w') as file:
         json.dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """
+    Load from JSON File Function
+
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file to load
+
+    Returns:
+        object: Python data structure created from the JSON file
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
