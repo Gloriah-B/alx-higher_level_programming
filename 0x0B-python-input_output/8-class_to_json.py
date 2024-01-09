76cbbe130c782392d6fad9513917cbf1d470287d
Gloriah-B <Gloriah-B@github.com>
2024-01-09 10:54:32 -0800

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class to JSON module.
+
+Contains function to serialize class instance into JSON-compatible dict
+"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description of object for JSON serialization
+
+    Args:
+        obj: The object to serialize.
+
+    Returns:
+        dict: A dictionary reps of object suitable for JSON serialization
+    """
+    # Function implementation here
