22d2f08b164d75888a47038c178c557c907ab6fc
Gloriah-B <Gloriah-B@github.com>
2024-01-10 13:02:33 -0800

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+class MyInt(int):
+    """
+    A class representing a rebel integer, inheriting from int.
+    """
+
+    def __eq__(self, other):
+        """
+        Inverts the behavior of the == operator.
+
+        Args:
+        - other: The other value to compare
+
+        Returns:
+        - bool: True if not equal; False if equal
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Inverts the behavior of the != operator.
+
+        Args:
+        - other: The other value to compare
+
+        Returns:
+        - bool: True if equal; False if not equal
+        """
+        return super().__eq__(other)
