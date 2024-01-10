de5a1e7bc930b6211bfa25432e983bd5430c494a
Gloriah-B <Gloriah-B@github.com>
2024-01-10 11:59:45 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Module provides functn to look up avlble attributes and method of object
+"""
+
+def lookup(obj):
+    """
+    Return list containing available attributes and method of given object
+
+    Args:
+    - obj: Any Python object
+
+    Returns:
+    - List: A list containing attributes and methods of the object.
+    """
+    return dir(obj)
