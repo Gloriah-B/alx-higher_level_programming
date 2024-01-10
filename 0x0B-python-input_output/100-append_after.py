86ecddd7660fa4cf46a8325a644100cdcb4d9de4
Gloriah-B <Gloriah-B@github.com>
2024-01-10 08:10:22 -0800

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,25 +2,29 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Append textline after each line containing specific string in a file
+    Append line of text after each line with specific string in a file
 
     Args:
     - filename (str): Name of the file to modify.
-    - search_string (str): String to search for in each line of the file
-    - new_string (str): String to append after each line of search_string
+    - search_string (str): String to search for in each line of the file.
+    - new_string (str): String to append aftr each line with search_string
 
     Note:
     - Uses the with statement to manage file handling.
     """
-    with open(filename, 'r+') as file:
-        lines = file.readlines()
-        file.seek(0)
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        return
+
+    with open(filename, 'w') as file:
         for line in lines:
             file.write(line)
             if search_string in line:
                 file.write(new_string)
-        file.truncate()
 
 
 if __name__ == "__main__":
+    # Example usage:
     append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
