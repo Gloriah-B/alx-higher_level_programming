11870f34ab7943b5606b2cedf4fdbaa784fd74cf
Gloriah-B <Gloriah-B@github.com>
2024-01-10 11:38:34 -0800

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,46 +4,42 @@
 
 
 class Student:
-    """Defines a student with attributes: first_name, last_name, and age."""
+    """Defines a Student by public instance attributes."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with given attributes."""
+        """Initialize Student instance with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of a Student instance.
+        Retrieve a dictionary representation of a Student instance.
 
-        If attrs is a list of strings, only attribute names contained in this
-        list must be retrieved. Otherwise, all attributes must be retrieved.
+        Args:
+            attrs (list, optional): List of strings representing attributes.
+                If provided, only these attributes will be retrieved.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
         """
-        if attrs is not None and all(isinstance(attr, str) for attr in attrs):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+                }
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance with a dictionary.
+        Replace all attributes of the Student instance with provided JSON.
 
-        Assumes json will always be a dictionary where each key is a public
-        attribute name and each value is the value of the public attribute.
+        Args:
+            json (dict): Dictionary representing attributes to replace.
+
+        Returns:
+            None
         """
         for key, value in json.items():
             setattr(self, key, value)
-
-
-# Correct output - reload with json of Student("John", "Snow", 25)
-student = Student("John", "Snow", 25)
-json_data = student.to_json()
-student.reload_from_json(json_data)
-expected_output = {'first_name': 'John', 'last_name': 'Snow', 'age': 25}
-print(student.to_json() == expected_output)
-
-
-# Correct output - { 'firstname': "Kevin", 'lastname': "Mc Joe", 'age': 43 }
-student = Student("John", "Snow", 25)
-json_data = {'first_name': "Kevin", 'last_name': "Mc Joe", 'age': 43}
-student.reload_from_json(json_data)
-print(student.to_json() == json_data)
