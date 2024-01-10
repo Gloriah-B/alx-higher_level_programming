b88faedffb2a6543163a4ab8806007a3c7d0bc6d
Gloriah-B <Gloriah-B@github.com>
2024-01-10 10:48:21 -0800

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 class Student:
-    """Class defining a student."""
-
     def __init__(self, first_name, last_name, age):
-        """Initialize Student with first name, last name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieve a dictionary representation of a Student instance."""
         return {
             'first_name': self.first_name,
             'last_name': self.last_name,
