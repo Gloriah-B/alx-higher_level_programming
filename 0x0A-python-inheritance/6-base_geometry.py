83690b5ff0079d1475ef87c13d0fa676355531d6
Gloriah-B <Gloriah-B@github.com>
2024-01-10 12:30:14 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class defining basic geometry operations.
+    """
+
+    def area(self):
+        """
+        Public instance methd tht raises xceptin indicate area() nt implemented
+        """
+        raise Exception("area() is not implemented")
