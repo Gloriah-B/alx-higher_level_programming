b9ca16c11898523694eab46368b3590a55a0e249
Gloriah-B <Gloriah-B@github.com>
2024-01-10 13:12:50 -0800

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class called MyInt, inheriting from int.
+"""
+
+
 class MyInt(int):
     """
     A class representing a rebel integer, inheriting from int.
