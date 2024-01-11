c19f1c149930cf2e230ed1c2912b09beb04aa5dd
Gloriah-B <Gloriah-B@github.com>
2024-01-11 10:33:12 -0800

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -2,7 +2,7 @@
 """
 Module that defines the Student class.
 
-Module includes the defn of the Student class, which reps a student
+Module includes the definition of the Student class, which reps a student
 with attributes such as first name, last name, and age.
 """
 
@@ -30,26 +30,15 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def __str__(self):
+    def to_json(self):
         """
-        Returns a string representation of the Student instance.
+        Retrieves a dictionary representation of a Student instance.
 
         Returns:
-            str: A string representation of the Student instance.
+            dict: A dictionary representation of the Student instance.
         """
-        return f"Student('{self.first_name}', '{self.last_name}', {self.age})"
-
-# Example usage:
-# Create a Student instance
-
-
-student1 = Student("Tom", "Smith", 89)
-print(student1.to_json())
-
-# Create another Student instance
-student2 = Student("I have a long name", "A very long name, not you?", -89)
-print(student2.to_json())
-
-# Create another Student instance
-student3 = Student("", "", 0)
-print(student3.to_json())
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
