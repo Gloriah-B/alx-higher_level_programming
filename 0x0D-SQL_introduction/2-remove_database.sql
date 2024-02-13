20493cf0a43f3f546b81382683452a3cb4e50626
Gloriah-B <Gloriah-B@github.com>
2024-02-13 09:13:10 -0800

    2-remove_database.sql

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Script: delete_database_if_exists.sql
+-- Description: Deletes the database hbtn_0c_0 in MySQL server if it exists
+
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
