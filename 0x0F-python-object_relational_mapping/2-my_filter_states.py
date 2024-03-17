a688426e0dba3e536c4867e5e6ecf7e17d080d26
Gloriah-B <Gloriah-B@github.com>
2024-03-17 12:38:18 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,37 +4,14 @@ import MySQLdb
 import sys
 
 
-def search_states(username, password, database_name, state_name):
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database_name,
-        port=3306
-    )
-
-    cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py < username > < password >
-              < database_name > < state_name >") sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    search_states(username, password, database_name, state_name)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
