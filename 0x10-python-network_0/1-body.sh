803122ef20675359557de556415f96ac82cbc1e3
Gloriah-B <Gloriah-B@github.com>
2024-04-08 11:08:34 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This takes URL as input, sends GET request to URL using curl, displays body response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -L "${1}"
