3fb8ab313039c3f4f1515bf9174415cb21ac8b09
Gloriah-B <Gloriah-B@github.com>
2024-04-08 10:10:56 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Script to fetch the size of the response body from a given URL using curl
-
-
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
-
+# This script takes a URL as input, sends a request to that URL using curl in silent mode, and displays the size of the response body in bytes.
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
